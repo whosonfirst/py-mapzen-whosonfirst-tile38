1ae2e22c415a35f6d595cab80e31e50a8420bc2a
thisisaaronland <whosonfirst@mapzen.com>
2016-11-10 01:01:59 +0000

    add basic intersects functionality; yield all the results rather than creating a giant list; bump to 0.031

--- mapzen/whosonfirst/tile38/__init__.py
+++ mapzen/whosonfirst/tile38/__init__.py
@@ -71,7 +71,6 @@ class whosonfirst_client(client):
     def nearby_paginated(self, lat, lon, r, **kwargs):
 
         rsp = None
-        objects = []
         cursor = 0
 
         while True:
@@ -79,15 +78,75 @@ class whosonfirst_client(client):
             kwargs['cursor'] = cursor
             rsp = self.nearby(lat, lon, r, **kwargs)
 
-            objects.extend(rsp['objects'])
+            if not rsp['ok']:
+                logging.error(rsp['err'])
+                break
+
+            for o in rsp['objects']:
+                yield o
 
             cursor = rsp.get('cursor', 0)
 
             if cursor == 0:
                 break
 
-        rsp['objects'] = objects
-        return rsp
+    def intersects(self, swlat, swlon, nelat, nelon, **kwargs):
+
+        cursor = kwargs.get('cursor', 0)
+        filters = kwargs.get('filters', {})
+        possible = ('wof:id', 'wof:placetype_id')
+
+        where = []
+
+        for k in possible:
+
+            v = filters.get(k, None)
+
+            if v:
+                where.append("WHERE %s %s %s" % (k, v, v))
+
+        cmd = [ "INTERSECTS", self.collection ]
+
+        if cursor != 0:
+            cmd.extend(["CURSOR %s" % cursor])
+
+        if len(where):
+            cmd.extend(where)
+
+        cmd.extend([
+            "BOUNDS",
+            "%0.6f" % swlat,
+            "%0.6f" % swlon,
+            "%0.6f" % nelat,
+            "%0.6f" % nelon
+        ])
+        
+        cmd = " ".join(map(str, cmd))
+        logging.debug(cmd)
+
+        return self.do(cmd)
+
+    def intersects_paginated(self, swlat, swlon, nelat, nelon, **kwargs):
+
+        rsp = None
+        cursor = 0
+
+        while True:
+
+            kwargs['cursor'] = cursor
+            rsp = self.intersects(swlat, swlon, nelat, nelon, **kwargs)
+
+            if not rsp['ok']:
+                logging.error(rsp['err'])
+                break
+
+            for o in rsp['objects']:
+                yield o
+
+            cursor = rsp.get('cursor', 0)
+
+            if cursor == 0:
+                break
 
     def rsp2features(self, rsp, **kwargs):
 
