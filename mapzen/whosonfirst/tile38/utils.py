6a478a98a0cf17a9be1e8be0d7259ae6e28115e4
thisisaaronland <whosonfirst@mapzen.com>
2016-11-11 01:37:48 +0000

    stub out proper whereami functions

--- mapzen/whosonfirst/tile38/utils.py
+++ mapzen/whosonfirst/tile38/utils.py
@@ -4,6 +4,9 @@ import mapzen.whosonfirst.utils
 
 import os
 
+# everything in here is early days and wrapped up in a big sweater of experimental
+# (20161110/thisisaaronland)
+
 def append_parent_and_hierarchy(t38_client, feature, **kwargs):
 
     props = feature['properties']
@@ -43,6 +46,9 @@ def append_parent_and_hierarchy(t38_client, feature, **kwargs):
             id = pr['wof:id']
             repo = pr['wof:repo']
 
+            # see below - this hasn't been rolled in to production yet
+            # hierarchies.extend(pr['wof:hierarchy'])
+
             # it might make more sense overall to store the hierarchies in T38
             # itself - it sort of depends on whether it makes sense to use T38
             # for doing point-in-poly at all (20161110/thisisaaronland)
@@ -63,5 +69,69 @@ def append_parent_and_hierarchy(t38_client, feature, **kwargs):
     props['wof:parent_id'] = parent_id
     props['wof:hierarchy'] = hierarchies
 
+def whereami(t38_client, feature, **kwargs):
 
+    props = feature['properties']
+    placetype = props['wof:placetype']
+
+    lat, lon = mapzen.whosonfirst.pip.utils.reverse_geocoordinates(feature)
+
+    return whereami_by_coord(t38_client, lat, lon, placetype, **kwargs)
+
+def whereami_by_coord(t38_client, lat, lon, placetype, **kwargs):
+
+    pt = mapzen.whosonfirst.placetypes.placetype(placetype)
+
+    hierarchy = {}
+    match = None
+
+    roles = kwargs.get('roles', ['common'])
+
+    for a in pt.ancestors(roles):
+
+        filters = {
+            'wof:placetype': str(a)
+        }
+
+        pip_kwargs = {
+            'filters': filters,
+            'as_feature': True
+        }
+
+        rsp = t38_client.point_in_polygon(lat, lon, **pip_kwargs)
+        rsp = list(rsp)
+        
+        if len(rsp) != 1:
+            hierarchy[ str(a) ] = -1
+            continue
+
+        match = rsp[0]
+        break
+
+    if match:
+
+        # see notes above inre storing the hierarchy in T38 itself
+        # (20161110/thisisaaronland)
+
+        pr = match['properties']
+        id = pr['wof:id']
+        repo = pr['wof:repo']
+
+        root = os.path.join(kwargs['data_root'], repo)
+        data = os.path.join(root, 'data')
+
+        f = mapzen.whosonfirst.utils.load(data, id)            
+
+        # see this - we are just ignoring instances where there are
+        # 0 or > 1 hierarchies - we should do something about that...
+        # (20161110/thisisaaronland)
+
+        for k, v in f['properties']['wof:hierarchy'][0].items():
+
+            if hierarchy.get(k):
+                continue
+            
+            hierarchy[k] = v
+
+    return hierarchy
 
